15411aa8f9a31ced81d1ddef70cdb8e74e65e27c
Hyung Kim <hjkmines@gmail.com>
2020-06-01 00:50:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,8 @@
 # Write your solution here
 
-time_in_seconds = Time.now.to_s 
-convert_to_integer = time_in_seconds.to_i 
+time_in_seconds = Time.now.to_i 
 
-if convert_to_integer % 2 == 0 
+if time_in_seconds % 2 == 0 
   puts "Even!"
 else 
   puts "Odd!"
